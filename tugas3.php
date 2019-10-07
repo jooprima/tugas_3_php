912178224b870b12d4a55a384417f4ba4e0e5e26
yohanes_prima <jooprima@gmail.com>
2019-10-07 10:45:39 +0700

    Tugas 3 php
    tugas ini dikumpulkan untuk niomic

---
+++ tugas3.php
@@ -0,0 +1,10 @@
+<?php
+$x=360;
+$y=90;
+$z=($x/$y)*60;
+
+echo "Jarak kota A-Z = $x Km<br>
+      Kecepatan Motor = $y Km/jam<br>
+      Waktu tempuh = ?<br>
+      Jawaban : $z menit";
+ ?>
---
+++ tugas3_2.php
@@ -0,0 +1,11 @@
+<?php
+$a=150000;
+$b=12.5;
+$c=(($b/100)*$a)+$a;
+
+echo "Tabungan awal = Rp $a<br>
+      Bunga setahun = $b %<br>
+      Total tabungan setahun = ?<br>
+      Jawaban : Rp $c";
+
+ ?>
